54e61faf169e4ef79433e5bc2bc9e091073e4390
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-02-05 14:18:24 +0530

    Update Selectionsort.c

--- Selectionsort.c
+++ Selectionsort.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 int main()
-{ int n,i,j,A[20],temp;
+{
+    int n,i,a[10],temp,j,min;
     printf("Enter n :");
     scanf("%d",&n);
-    printf("Enter the elements :");
+    printf("Enter elements :");
     for(i=0;i<n;i++)
     {
-        scanf("%d",&A[i]);
+    scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n-1;i++)
     {
+        min=i;
         for(j=i+1;j<n;j++)
         {
-            if(A[j]<A[i])
+            if(a[min]>a[j])
             {
-                temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
+                min=j;
             }
         }
+        temp=a[i];
+        a[i]=a[min];
+        a[min]=temp;
     }
+    printf("SORTED ARRAY\n");
     for(i=0;i<n;i++)
     {
-        printf("%d",A[i]);
+        printf("%d\t",a[i]);
     }
 }
